c70c47705ca124e106ec018986761b84ebbb9690
Nghia <vnghia29>
2023-05-11 04:25:54 +0700

    Payment commit

--- app/src/main/java/com/example/it_food/activity/PaymentMedthod.java
+++ app/src/main/java/com/example/it_food/activity/PaymentMedthod.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.it_food.R;
 
-public class MainActivity extends AppCompatActivity {
+public class PaymentMedthod extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_password);
+        setContentView(R.layout.activity_payment_medthod);
     }
 }
